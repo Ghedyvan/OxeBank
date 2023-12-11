ee45204cfa3ff844478ee712167d7df4b3374bf3
Ghedyvan <gvbs@ic.ufal.br>
2023-12-11 11:54:57 -0300

    Página inicial ok

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,17 +3,17 @@
 </script>
 
 <div class="w-full h-[88px] justify-center flex bg-hero bg-cover border-b-gray-50 border-b border-opacity-30">
-	<div class="max-w-[880px] flex justify-between w-full">
+	<div class="max-w-[1216px] flex justify-between w-full">
 		<img src="/assets/logoWhite.svg" alt="logo arkademy" class="w-[180px]" />
 		<ul class="flex gap-[40px] items-center">
 			<li>
-				<a href="#" class="text-white">Produtos</a>
+				<a href="/" class="text-white">Produtos</a>
 			</li>
 			<li>
-				<a href="#" class="text-white">Quem somos</a>
+				<a href="/" class="text-white">Quem somos</a>
 			</li>
 			<li>
-				<a href="#" class="text-white">Ajuda</a>
+				<a href="/" class="text-white">Ajuda</a>
 			</li>
 			<li>
 				<Button textButton={'Abra sua conta digital'} backgroundColor={'red'}/>
--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -2,23 +2,28 @@
 	import Button from './Button.svelte';
 </script>
 
-<div class="h-[600px] bg-hero bg-cover w-full justify-center flex">
-	<div class="pt-[87px] max-w-[880px] justify-between flex w-full">
+<div class="h-[675px] bg-hero bg-cover w-full justify-center flex">
+	<div class="pt-[87px] max-w-[1216px] justify-between flex w-full">
 		<div class="max-w-[560px]">
-			<p class="text-white text-2xl">Abra sua conta, é só baixar o app!</p>
-			<h1 class="text-white text-[54px] font-semibold leading-[65px] mb-[24px] pt-[32px]">
-				A conta digital certa pra cuidar bem do seu dinheiro
+			<p class="text-[#429EBD] font-semibold text-2xl flex">
+				Que 2024 seja radiante e feliz! <span class="pl-4">
+					<img src="/assets/iconFesta.svg" alt="Icone de festa" />
+				</span>
+			</p>
+			<h1 class="text-white text-[54px] font-semibold leading-[65px] mb-[46px] pt-[32px]">
+				Inicie o ano com a conta digital <span class="text-[#F7AD19]">perfeita</span> para
+				<span class="text-[#F7AD19]">seu dinheiro</span>
 			</h1>
 			<div class="flex gap-[24px]">
 				<Button
-					classList={'bg-[#FFC800] !text-gray-500 text-base'}
+					classList={'!bg-[#F7AD19] !text-[#033650] text-base'}
 					textButton={'Abra sua conta digital'}
 				/>
 			</div>
-			<div class="w-full flex justify-between mt-[59px]">
+			<div class="w-full flex justify-between mt-[64px]">
 				<div class="w-full max-w-[148px] flex justify-between">
 					<div class="w-[48px]">
-						<img src="/assets/elipseHero.png" alt="hero elipse" class="absolute" />
+						<img src="/assets/elipseHero.svg" alt="hero elipse" class="absolute" />
 						<img
 							src="/assets/cardIconHero.svg"
 							alt="hero elipse"
@@ -29,7 +34,7 @@
 				</div>
 				<div class="w-full max-w-[148px] flex justify-between">
 					<div class="w-[48px]">
-						<img src="/assets/elipseHero.png" alt="hero elipse" class="absolute" />
+						<img src="/assets/elipseHero.svg" alt="hero elipse" class="absolute" />
 						<img
 							src="/assets/zeroIconHero.svg"
 							alt="hero elipse"
@@ -40,7 +45,7 @@
 				</div>
 				<div class="w-full max-w-[148px] flex justify-between">
 					<div class="w-[48px]">
-						<img src="/assets/elipseHero.png" alt="hero elipse" class="absolute" />
+						<img src="/assets/elipseHero.svg" alt="hero elipse" class="absolute" />
 						<img
 							src="/assets/moneyIconHero.svg"
 							alt="hero elipse"
@@ -52,20 +57,21 @@
 			</div>
 		</div>
 		<div>
-			<h1
-				class="opacity-40 text-white text-[48px] max-w-[500px] font-semibold w-full absolute right-50"
-			>
+			<h1 class=" text-[#3687A2] text-[48px] max-w-[500px] font-semibold w-full absolute">
 				Banco 100% digital
 			</h1>
 			<div class="relative">
 				<img src="/assets/circleHero.svg" alt="hero phone" class="mt-[90px]" />
-
 				<img
-					src="/assets/cardBackHeroD.png"
+					src="/assets/cardBackHero.svg"
 					alt="hero phone"
-					class="absolute top-[-10px] right-[-110px]"
+					class="absolute bottom-0 right-[37px]"
 				/>
-				<img src="/assets/cardFrontHero.png" alt="hero phone" class="absolute top-20 right-20" />
+				<img
+					src="/assets/cardFrontHero.svg"
+					alt="hero phone"
+					class="absolute top-[29px] left-[-88px]"
+				/>
 			</div>
 		</div>
 	</div>
